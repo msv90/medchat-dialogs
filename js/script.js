c511ee2c1c570b4fc3ccf6ade4cf914967edd3d9
Пользователь <mike.developer90@gmail.com>
2020-05-18 19:01:35 +0300

    1. Changed styles of chat. 2. Scroll-y. 3. Images and selects in messages.

---
+++ js/script.js
@@ -0,0 +1,3 @@
+/* Сообщения диалога - Скролл сразу внизу (приего наличии) */
+let dialogDetailesMessages = document.querySelector('#dialog-detailed__messages');
+dialogDetailesMessages.scrollTop = dialogDetailesMessages.scrollHeight;
