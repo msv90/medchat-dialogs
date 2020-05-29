6b992d7449c8aad7cdd88a897ee3356ba6f94e28
Пользователь <mike.developer90@gmail.com>
2020-05-29 12:28:25 +0300

    Dialogs and Settings frames

--- js/script.js
+++ js/script.js
@@ -1,3 +1,20 @@
-/* Сообщения диалога - Скролл сразу внизу (приего наличии) */
+/* Сообщения диалога - Скролл сразу внизу (при его наличии) */
 let dialogDetailesMessages = document.querySelector('#dialog-detailed__messages');
 dialogDetailesMessages.scrollTop = dialogDetailesMessages.scrollHeight;
+
+/* Переключение между фреймами Диалогов */
+let toggleMenu = document.querySelector('#frame-dialogs__header-toggle-menu');
+let arrowBack = document.querySelector('#frame-settings__header-icon-arrow');
+
+let frameDialogs = document.querySelector('#frame-dialogs');
+let frameSettings = document.querySelector('#frame-settings');
+
+toggleMenu.addEventListener('click', function() {
+    frameDialogs.style.display = "none";
+    frameSettings.style.display = "flex";
+});
+
+arrowBack.addEventListener('click', function() {
+    frameSettings.style.display = "none";
+    frameDialogs.style.display = "flex";
+});
