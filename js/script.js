c4df451c02d8c09fb57e2f3a1a59fd7b2a4c6274
Пользователь <mike.developer90@gmail.com>
2020-05-29 14:58:59 +0300

    1. Profile frame. 2. Font-size reducing

--- js/script.js
+++ js/script.js
@@ -4,17 +4,30 @@ dialogDetailesMessages.scrollTop = dialogDetailesMessages.scrollHeight;
 
 /* Переключение между фреймами Диалогов */
 let toggleMenu = document.querySelector('#frame-dialogs__header-toggle-menu');
-let arrowBack = document.querySelector('#frame-settings__header-icon-arrow');
+let arrowBackSettings = document.querySelector('#frame-settings__header-icon-arrow');
+let arrowBackProfile = document.querySelector('#frame-profile__header-icon-arrow');
+let linkProfile = document.querySelector('#link-profile');
 
 let frameDialogs = document.querySelector('#frame-dialogs');
 let frameSettings = document.querySelector('#frame-settings');
+let frameProfile = document.querySelector('#frame-profile');
 
 toggleMenu.addEventListener('click', function() {
     frameDialogs.style.display = "none";
     frameSettings.style.display = "flex";
 });
 
-arrowBack.addEventListener('click', function() {
+arrowBackSettings.addEventListener('click', function() {
     frameSettings.style.display = "none";
     frameDialogs.style.display = "flex";
+});
+
+linkProfile.addEventListener('click', function() {
+    frameSettings.style.display = "none";
+    frameProfile.style.display = "flex";
+});
+
+arrowBackProfile.addEventListener('click', function() {
+    frameProfile.style.display = "none";
+    frameSettings.style.display = "flex";
 });
